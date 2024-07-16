77f58e1fe751daca5a7ed384d45a4475e67dfc5b
sunmon <blackmilk274@gmail.com>
2024-07-16 23:04:45 +0900

    add example codes

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useRef, useState } from 'react'
+import {Engine, Render, Runner, Bodies, Composite} from 'matter-js'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const containerRef = useRef<HTMLDivElement>(null);
+  useEffect(() => { 
+
+    const engine = Engine.create();
+    const render = Render.create({
+      // element: document.body,
+      element: containerRef.current,
+      engine: engine,
+      options: {
+        width: 800,
+        height: 600,
+        wireframes: false
+      }
+    });
+  
+    const boxA = Bodies.rectangle(400, 200, 80, 80);
+    const boxB = Bodies.rectangle(450, 50, 80, 80);
+    const ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
+  
+    Composite.add(engine.world, [boxA, boxB, ground]);
+  
+    Render.run(render)
+  
+    const runner = Runner.create();
+    Runner.run(runner, engine);
+  
+  }, [])
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div ref={containerRef}>
+
+
+       </div>
     </>
   )
 }
